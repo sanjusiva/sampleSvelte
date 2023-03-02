491e9b1eac5ff66901371ad0d69f4b5ecf1458c8
sanjitha.siva <sanjitha.siva@aspiresys.com>
2023-03-02 16:43:52 +0530

    svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<h1>Adapters</h1>
+<p>Before you can deploy your SvelteKit app, you need to adapt it for your deployment target. Adapters are small plugins that take the built app as input and generate output for deployment.</p>
